f16ecc5c66b0d3d4fa4fb8f7ce02bdf1c82d4bf1
danic-jct <41348513+danic-jct@users.noreply.github.com>
2021-01-04 11:22:47 -0800

    Create macos_remove_flash.sh

---
+++ macos_remove_flash.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+##Pulled from https://www.jamf.com/jamf-nation/discussions/15266/flash-removal-script
+
+dmgfile="uninstall_flash_player_osx.dmg"
+volname="Flash"
+logfile="/Library/Logs/FlashRemovalScript.log"
+
+url="https://fpdownload.macromedia.com/get/flashplayer/current/support/uninstall_flash_player_osx.dmg"
+
+
+    currentinstalledver=`/usr/bin/defaults read "/Library/Internet Plug-Ins/Flash Player.plugin/Contents/version" CFBundleShortVersionString`
+    if [ "${currentinstalledver}" ]; then
+        /bin/echo "`date`: Downloading uninstaller." >> ${logfile}
+        /usr/bin/curl -s -o /tmp/$dmgfile $url
+        /bin/echo "`date`: Mounting uninstaller disk image." >> ${logfile}
+        /usr/bin/hdiutil attach /tmp/$dmgfile -nobrowse -quiet
+        /bin/echo "`date`: Uninstalling..." >> ${logfile}
+        /Volumes/Flash\ Player/Adobe\ Flash\ Player\ Uninstaller.app/Contents/MacOS/Adobe\ Flash\ Player\ Install\ Manager -uninstall> /dev/null
+        /bin/sleep 10
+        /bin/echo "`date`: Unmounting uninstaller disk image." >> ${logfile}
+        /usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep ${volname} | awk '{print $1}') -quiet
+        /bin/sleep 10
+        /bin/echo "`date`: Deleting disk image." >> ${logfile}
+        /bin/rm /tmp/${dmgfile}
+    else
+        /bin/echo "`date`: Flash is not installed" >> ${logfile}
+        /bin/echo "--" >> ${logfile}
+    fi
